9872b7b5c546067eeb240231a1363cf61fe93b1d
antosh <tomatomoving@mail.ru>
2025-05-28 11:57:56 +0300

    feat: changed file structure to FSD-style

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,31 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@widgets': path.resolve(__dirname, 'src/widgets'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@entities': path.resolve(__dirname, 'src/entities'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
     },
   },
+  // использовать когда понадобятся переменные и миксины
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `
+  //       @import "@/scss/_variables.scss";
+  //       @import "@/scss/_mixins.scss";
+  //     `,
+  //     },
+  //   },
+  // },
 })
