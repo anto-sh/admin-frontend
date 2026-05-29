7440f66f0a7eef5fd73a14afa679d5ed02e420f0
antosh <tomatomoving@mail.ru>
2026-05-29 19:09:58 +0300

    refactor: removed unused headers prop from ApiResponseDto type

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -2,7 +2,6 @@
 export interface ApiResponseDto<T = unknown> {
   status: 'success' | 'error'
   code: number // HTTP статус-код
-  headers?: Record<string, string> // Дополнительные заголовки
   message: string
   data?: T // Основные данные ответа
   timestamp?: string // Дата формирования ответа
